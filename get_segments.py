c4a21532bc50f452c6cd09fabab1c8a985822f4b
bennlich <bennlich@gmail.com>
2015-04-28 15:12:38 -0700

    Add segment_detection dummy and get_segments script

---
+++ get_segments.py
@@ -0,0 +1,50 @@
+"""
+Description:
+  Given a seismogram and a FeatureCollection of intersections, calculates
+  the segments that compose the signal. Outputs a geojson FeatureCollection
+  with one feature per segment. Each feature has a LineString geometry and
+  properties (TODO: what properties?).
+
+Usage:
+  get_segments.py --image <filename> --intersections <filename> --output <filename> [--debug]
+  get_segments.py -h | --help
+
+Options:
+  -h --help                   Show this screen.
+  --image <filename>          Filename of grayscale input image.
+  --intersections <filename>  Filename of intersection metadata.
+  --output <filename>         Filename of geojson output.
+  -d --debug                  Save intermediate steps as images for inspection in debug/.
+
+"""
+
+from docopt import docopt
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    in_file = arguments["--image"]
+    intersections_file = arguments["--intersections"]
+    out_file = arguments["--output"]
+    debug = arguments["--debug"]
+
+    if (in_file and out_file and intersections_file):
+      from lib.timer import timeStart, timeEnd
+      from lib.load_image import get_image
+      from lib.load_geojson import get_features
+      from lib.segment_detection import get_segments
+      from lib.segment_detection import save_segments_as_geojson
+
+      timeStart("DONE", immediate=False)
+      image = get_image(in_file)
+      intersections = get_features(intersections_file)
+      
+      timeStart("get segments")
+      segments = get_segments(image, intersections)
+      timeEnd("get segments")
+      
+      print segments
+      
+      save_segments_as_geojson(segments, out_file)
+      timeEnd("DONE", immediate=False)
+    else:
+      print(arguments)
--- lib/segment_detection.py
+++ lib/segment_detection.py
@@ -1,1 +1,24 @@
-# stub
+from timer import timeStart, timeEnd
+
+from numpy.random import rand, randint
+import geojson
+
+def get_segments(image, intersections):
+  # Generate dummy segments for now
+  shape = image.shape
+  min_num_points = 3
+  max_num_points = 15
+  num_lines = randint(50, 200)
+  line_array = [(shape * rand(randint(min_num_points, max_num_points), 2)).astype(int) for i in range(num_lines)]
+  line_array = [map(tuple, line) for line in line_array]
+  return line_array
+
+def save_segments_as_geojson(segments, filepath):
+  timeStart("saving to "+ filepath)
+  for line in segments:
+    print line
+  segment_features = [ geojson.Feature(geometry = geojson.LineString(line)) for line in segments ]
+  collection = geojson.FeatureCollection(segment_features)
+  with open(filepath, 'w') as outfile:
+    geojson.dump(collection, outfile)
+  timeEnd("saving to "+ filepath)
