e4951f08a648916f996d831d91b1faa113d78aa8
bennlich <bennlich@gmail.com>
2015-04-29 14:34:54 -0700

    Add segment assignment dummy

---
+++ get_segment_assignments.py
@@ -0,0 +1,47 @@
+"""
+Description:
+  Given a list of segments and a list of meanlines, assigns each segment to
+  a meanline. Outputs a json object of the form:
+    {
+      meanline_id: [ segment_id, another_segment_id, ... ],
+      ...
+    }
+
+Usage:
+  get_segment_assignments.py --meanlines <filename> --segments <filename> --output <filename>
+  get_segment_assignments.py -h | --help
+
+Options:
+  -h --help               Show this screen.
+  --meanlines <filename>  Filename of meanline metadata.
+  --segments <filename>   Filename of segment metadata.
+  --output <filename>     Filename of json output.
+
+"""
+
+from docopt import docopt
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    segments_file = arguments["--segments"]
+    meanlines_file = arguments["--meanlines"]
+    out_file = arguments["--output"]
+
+    if (segments_file and meanlines_file):
+      from lib.timer import timeStart, timeEnd
+      from lib.load_geojson import get_features
+      from lib.segment_assignment import assign_segments_to_meanlines
+      from lib.segment_assignment import save_assignments_as_json
+
+      timeStart("DONE", immediate=False)
+      segments = get_features(segments_file)
+      meanlines = get_features(meanlines_file)
+      
+      timeStart("assign segments to meanlines")
+      assignments = assign_segments_to_meanlines(segments, meanlines)
+      timeEnd("assign segments to meanlines")
+      
+      save_assignments_as_json(assignments, out_file)
+      timeEnd("DONE", immediate=False)
+    else:
+      print(arguments)
--- lib/segment_assignment.py
+++ lib/segment_assignment.py
@@ -1,1 +1,20 @@
-# stub
+from timer import timeStart, timeEnd
+
+from random import sample, randint
+import json
+
+def assign_segments_to_meanlines(segments, meanlines):
+  # Generate dummy assignments for now
+  meanline_ids = [line.id for line in meanlines.features]
+  segment_ids = [segment.id for segment in segments.features]
+  avg_segments_per_meanline = len(segment_ids) / len(meanline_ids)
+  assignments = {}
+  for meanline_id in meanline_ids:
+    assignments[meanline_id] = [segment_ids.pop(i) for i in sorted(sample(range(len(segment_ids)), randint(0, avg_segments_per_meanline)), reverse=True)]
+  return assignments
+
+def save_assignments_as_json(assignments, filepath):
+  timeStart("saving to "+ filepath)
+  with open(filepath, 'w') as outfile:
+    json.dump(assignments, outfile)
+  timeEnd("saving to "+ filepath)
