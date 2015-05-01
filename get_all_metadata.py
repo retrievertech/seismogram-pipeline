545fbcd84e999aeddf194f70f129e4e7a3fbd063
bennlich <bennlich@gmail.com>
2015-05-01 14:26:13 -0700

    Add scale to get_all_metadata

--- get_all_metadata.py
+++ get_all_metadata.py
@@ -8,19 +8,20 @@ Description:
     segment_assignments.json
 
 Usage:
-  get_all_metadata.py --image <filename> --output <directory>
+  get_all_metadata.py --image <filename> --output <directory> [--scale <scale>]
   get_all_metadata.py -h | --help
 
 Options:
   -h --help             Show this screen.
   --image <filename>    Filename of seismogram.
-  --output <directory>  Directory 
+  --output <directory>  Directory in which to save metadata.
+  --scale <scale>       1 for a full-size seismogram, 0.25 for quarter-size, etc. [default: 1]
 
 """
 
 from docopt import docopt
 
-def get_all_metadata(in_file, out_dir):
+def get_all_metadata(in_file, out_dir, scale=1):
   from lib.dir import ensure_dir_exists
   ensure_dir_exists(out_dir)
 
@@ -44,9 +45,9 @@ def get_all_metadata(in_file, out_dir):
   timeStart("ALL DONE", immediate=False)
 
   print "\n--ROI--"
-  get_roi(in_file, paths["roi"])
+  get_roi(in_file, paths["roi"], scale)
   print "\n--MEANLINES--"
-  get_meanlines(in_file, paths["meanlines"], paths["roi"])
+  get_meanlines(in_file, paths["meanlines"], paths["roi"], scale)
   print "\n--INTERSECTIONS--"
   # TODO: don't write the thresholded image to a file
   get_thresholded_image(in_file, paths["thresholded"])
@@ -63,8 +64,9 @@ if __name__ == '__main__':
   arguments = docopt(__doc__)
   in_file = arguments["--image"]
   out_dir = arguments["--output"]
+  scale = float(arguments["--scale"])
 
   if (in_file and out_dir):
-    get_all_metadata(in_file, out_dir)
+    get_all_metadata(in_file, out_dir, scale)
   else:
     print(arguments)
