733d8fc4eaa959ad470945cc29d9859118addaaf
bennlich <bennlich@gmail.com>
2015-05-06 16:29:41 -0700

    Add timers to resizeImage

--- resize_image.py
+++ resize_image.py
@@ -17,11 +17,20 @@ Options:
 from docopt import docopt
 from scipy.misc import imresize, imsave
 from lib.load_image import get_image
+from lib.timer import timeStart, timeEnd
 
 def resize_image(in_file, out_file, scale):
+  timeStart("load image")
   image = get_image(in_file)
+  timeEnd("load image")
+
+  timeStart("resize image")
   resized_image = imresize(image, scale)
+  timeEnd("resize image")
+
+  timeStart("save image")
   imsave(out_file, resized_image)
+  timeEnd("save image")
 
 if __name__ == '__main__':
   arguments = docopt(__doc__)
