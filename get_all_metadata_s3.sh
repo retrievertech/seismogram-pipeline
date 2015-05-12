45d2de74591f78e858481a42056af0ce472a8a6f
Marius Nita <m@mariusnita.com>
2015-05-12 19:59:54 +0300

    add status changes

--- get_all_metadata_s3.sh
+++ get_all_metadata_s3.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-dir=`mktemp -d` && \
+server=http://localhost:3000
+
+dir=`mktemp -d /tmp/seismo.XXXXX` && \
 echo "writing to $dir" && \
+wget $server/processing/setstatus/$1/1 #processing
 python get_all_metadata.py --image $2 --output $dir && \
 aws s3 cp --recursive $dir s3://wwssn-metadata/$1 && \
-rm -rf $dir
+rm -rf $dir && \
+wget $server/processing/setstatus/$1/3 #complete
