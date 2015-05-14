48c10c4bab5c9eb1e6582d1981e4dc24a5b3c88d
Marius Nita <m@mariusnita.com>
2015-05-14 14:54:03 +0300

    when given the dev param, copy metadata to ../seismogram-app/client/metadata instead of s3

--- get_all_metadata_s3.sh
+++ get_all_metadata_s3.sh
@@ -4,8 +4,12 @@ server=http://localhost:3000
 
 dir=`mktemp -d /tmp/seismo.XXXXX` && \
 echo "writing to $dir" && \
-wget $server/processing/setstatus/$1/1 #processing
+wget $server/processing/setstatus/$1/1 && \
 python get_all_metadata.py --image $2 --output $dir && \
-aws s3 cp --recursive $dir s3://wwssn-metadata/$1 && \
+if [ "$3" != "dev" ]; then
+  aws s3 cp --recursive $dir s3://wwssn-metadata/$1
+else
+  cp -r $dir ../seismogram-app/client/metadata/$1
+fi && \
 rm -rf $dir && \
 wget $server/processing/setstatus/$1/3 #complete
