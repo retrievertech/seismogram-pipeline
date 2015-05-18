fdb3dc217e8aef8eac0cb13f7f389451ee908a2c
Marius Nita <m@mariusnita.com>
2015-05-18 14:34:07 +0300

    add quotes around params

--- get_all_metadata_s3.sh
+++ get_all_metadata_s3.sh
@@ -5,7 +5,7 @@ server=http://localhost:3000
 dir=`mktemp -d /tmp/seismo.XXXXX` && \
 echo "writing to $dir" && \
 wget $server/processing/setstatus/$1/1 -O /dev/null && \
-python get_all_metadata.py --image $2 --output $dir && \
+python get_all_metadata.py --image "$2" --output "$dir" && \
 if [ "$3" != "dev" ]; then
   aws s3 cp --recursive $dir s3://wwssn-metadata/$1
 else
