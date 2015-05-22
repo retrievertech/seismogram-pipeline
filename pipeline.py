7fb4dfc55967ecbe7df700114bb571d5c6e5beee
bennlich <bennlich@gmail.com>
2015-05-22 01:29:28 -0700

    Add some print statements

--- pipeline.py
+++ pipeline.py
@@ -37,30 +37,41 @@ def analyze_image(in_file, out_file):
   timeEnd("read image")
 
   # get region of interest
+  print "\n--ROI--"
   boundary = get_boundary(image)
   lines = get_box_lines(boundary)
   corners = get_roi_corners(lines)
   roi_polygon = corners_to_geojson(corners)["geometry"]["coordinates"][0]
   masked_image = mask_image(image, roi_polygon)
 
-  # (flatten background?)
+  print "\n--FLATTEN BACKGROUND--"
   img_dark_removed, dark_pixels = flatten_background(img_gray, 0.95, 
                              return_background=True, mask=masked_image.mask)
   
   # get horizontal and vertical ridges
+  print "\n--RIDGES--"
   background = dark_pixels | local_min(img_gray)    
   ridges_h, ridges_v = find_ridges(img_dark_removed, background)
   ridges = ridges_h | ridges_v
+  
   # get binary image
+  print "\n--THRESHOLDING--"
   img_bin = binary_image(img_dark_removed, markers_trace=ridges,
                markers_background=background)
+  
   # get medial axis skeleton and distance transform
+  print "\n--SKELETONIZE--"
   img_skel, dist = medial_axis(img_bin, return_distance=True)
+  
   # detect intersections
+  print "\n--INTERSECTIONS--"
   intersections = find_intersections(img_bin, img_skel, dist)
+  
   # break into segments
+  print "\n--SEGMENTS--"
   segments = get_segments(img_gray, img_bin, img_skel, dist, intersections,
               ridges_h, ridges_v)    
+  
   timeStart("convert to geojson")
   segments_as_geojson = segments_to_geojson(segments)
   timeEnd("convert to geojson")
