987af93407bdf5d28da4350e1af0742c839c1656
Marius Nita <m@mariusnita.com>
2015-05-29 16:39:26 +0200

    split the "copy to s3" part into a new script

---
+++ copy_to_s3.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# copies a directory to the s3 metadata bucket
+#
+#  sh copy_to_s3.sh <image_name> <path_to_metadata_dir> [dev]
+#
+# if the third argument is "dev", the metadata is copied locally.
+# otherwise it's copied to the s3 bucket.
+
+# "xxxxxx_xxxx_xxxx_xx.png"
+image_name=$1
+# directory containing metadata
+dir=$2
+# a value of "edited" means to copy to the edited-metadata bucket
+# otherwise to the normal metadata bucket
+edited=$3
+# "dev" indicates development mode
+# production otherwise
+type=$4
+
+bucket_name=metadata
+
+if [ "$edited" == "edited" ]; then
+  bucket_name=edited-metadata
+fi
+
+if [ "$type" != "dev" ]; then
+  aws s3 cp --recursive $dir s3://wwssn-$bucket_name/$image_name
+else
+  mkdir -p ../seismogram-app/client/$bucket_name/$image_name && \
+  cp -r $dir/* ../seismogram-app/client/$bucket_name/$image_name
+fi
--- get_all_metadata_s3.sh
+++ get_all_metadata_s3.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
+# invokes the full pipeline on an image
+#
+#   sh get_all_metadata_s3.sh <image_name> <full_image_path> [dev]
+#
+# if the third argument is "dev", the data is copied locally.
+# otherwise it's copied to the s3 metadata bucket.
+
+# node server
 server=http://localhost:3000
+# "xxxxxx_xxxx_xxxx_xx.png"
+image_name=$1
+# full path to the image
+image_path=$2
+# "dev" is development, everything else is production
+type=$3
 
 dir=`mktemp -d /tmp/seismo.XXXXX` && \
 echo "writing to $dir" && \
-wget $server/processing/setstatus/$1/1 -O /dev/null && \
-python get_all_metadata.py --image "$2" --output "$dir" && \
-if [ "$3" != "dev" ]; then
-  aws s3 cp --recursive $dir s3://wwssn-metadata/$1
-else
-  cp -r $dir ../seismogram-app/client/metadata/$1
-fi && \
-rm -rf $dir && \
-wget $server/processing/setstatus/$1/3 -O /dev/null
+wget $server/processing/setstatus/$image_name/1 -O /dev/null && \
+python get_all_metadata.py --image "$image_path" --output "$dir" && \
+sh copy_to_s3.sh $image_name $dir normal $type && \
+wget $server/processing/setstatus/$image_name/3 -O /dev/null && \
+rm -rf $dir
