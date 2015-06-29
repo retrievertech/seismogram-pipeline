e3a88483665d5cccbdad3f082d379ea83b9722af
Marius Nita <m@mariusnita.com>
2015-06-29 14:31:06 -0600

    break code into methods; CSV output option

--- write_coords.py
+++ write_coords.py
@@ -4,14 +4,14 @@ Description:
   Extract segment endpoints from a segments.json file
 
 Usage:
-  write_coords.py --segments <filename> --output <filename>
+  write_coords.py --segments <filename> (--output <filename> | --output_csv <filaname>)
   write_coords.py -h | --help
 
 Options:
-  -h --help              Show this screen.
-  --segments <filename>  Filename of segments.json
-  --output <filename>    File to write to.
-
+  -h --help                Show this screen.
+  --segments <filename>    Filename of segments.json
+  --output <filename>      File to write geojson to.
+  --output_csv <filename>  File to write CSV to.
 """
 
 from docopt import docopt
@@ -23,7 +23,7 @@ from lib.timer import timeStart, timeEnd
 
 #features = get_features('C:\Users\Lowell\Documents\GitHub\seismogram-pipeline\metadata\segments.json')
 
-def get_endpoints(features):
+def get_endpoint_data(features):
   all_x = []
   all_y = []
 
@@ -36,10 +36,6 @@ def get_endpoints(features):
 
   average_y = []
   std_deviation_y = []
-  x1 = []
-  x2 = []
-  y1 = []
-  y2 = []
   startpoints = []
   endpoints = []
 
@@ -57,44 +53,58 @@ def get_endpoints(features):
     startpoints.append(startpoint)
     endpoints.append(endpoint)
 
-  segment_data = {
+  return {
+    "startpoints": startpoints,
+    "endpoints": endpoints,
+    "average_y": average_y,
+    "std_deviation_y": std_deviation_y
+  }
+
+def generate_geojson(data):
+  geojson_data = {
     "type": "FeatureCollection",
     "features": []
   }
 
-  for i in xrange(len(all_y)):
+  for i in xrange(len(data["endpoints"])):
     segment = {
-      "geometry": [startpoints[i], endpoints[i]],
+      "geometry": [data["startpoints"][i], data["endpoints"][i]],
       "properties": {
-        "average_y": average_y[i],
-        "standard_deviation": std_deviation_y[i],
+        "average_y": data["average_y"][i],
+        "standard_deviation": data["std_deviation_y"][i],
       }
     }
-    segment_data["features"].append(segment)
+    geojson_data["features"].append(segment)
 
-  with open('endpoints.csv', 'wb') as csvfile:
+  return geojson_data
+
+def write_geojson(out_filename, geojson_data):
+  with open(out_filename, 'w') as outfile:
+    geojson.dump(geojson_data, outfile)
+
+def write_csv(out_filename, data):
+  with open(out_filename, 'w') as csvfile:
     rowwriter = csv.writer(csvfile, delimiter=',')
-    for rows in xrange(len(endpoints)):
+    for i in xrange(len(data["endpoints"])):
       rowwriter = csv.writer(csvfile, delimiter=',')
-      rowwriter.writerow(startpoints[rows] + endpoints[rows])
+      rowwriter.writerow(data["startpoints"][i] + data["endpoints"][i])
 
-  #coord_length = []
-  #for size in xrange(len(all_x)):
-  #  coord_length.append(len(all_x[size]))
-  #coord_histogram = np.histogram(coord_length, bins = range(0, np.max(coord_length)))
-
-  return segment_data
+#coord_length = []
+#for size in xrange(len(all_x)):
+#  coord_length.append(len(all_x[size]))
+#coord_histogram = np.histogram(coord_length, bins = range(0, np.max(coord_length)))
 
 if __name__ == '__main__':
   arguments = docopt(__doc__)
   print arguments
   segments_file = arguments["--segments"]
-  print segments_file
-  out_file = arguments["--output"]
-  print out_file
+  geojson_out_file = arguments["--output"]
+  csv_out_file = arguments["--output_csv"]
 
   features = get_features(segments_file)
-  segment_data = get_endpoints(features)
+  data = get_endpoint_data(features)
 
-  with open(out_file, 'w') as outfile:
-    geojson.dump(segment_data, outfile)
+  if geojson_out_file is not None:
+    write_geojson(geojson_out_file, generate_geojson(data))
+  else:
+    write_csv(csv_out_file, data)
