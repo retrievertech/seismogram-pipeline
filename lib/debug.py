275e1d2800d6d2d8b3ee805eff2e1257a3706f74
Marius Nita <m@mariusnita.com>
2015-07-01 16:00:27 -0600

    add global count so the images are sorted in order of creation

--- lib/debug.py
+++ lib/debug.py
@@ -1,16 +1,21 @@
 from scipy import misc
 from dir import ensure_dir_exists
 
+def pad(number):
+  numstr = str(number)
+  return "0" + numstr if number < 10 else numstr
+
 class Debug:
 
   debug_dir = None
   active = False
+  global_count = 0
   stage_count = {}
 
   @classmethod
   def set_directory(cls, debug_dir):
     cls.debug_dir = debug_dir
-    
+
     if debug_dir is None:
       cls.active = False
     else:
@@ -23,5 +28,6 @@ class Debug:
       return
 
     count = cls.stage_count[stage] = cls.stage_count.get(stage, -1) + 1
-    filename = "%s.%s.%s.png" % (stage, count, name)
+    filename = "%s.%s.%s.%s.png" % (pad(cls.global_count), stage, pad(count), name)
     misc.imsave(cls.debug_dir+"/"+filename, img)
+    cls.global_count = cls.global_count + 1
