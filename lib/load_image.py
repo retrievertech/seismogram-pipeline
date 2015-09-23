36d9342a420cc939efa34472b48f6be261f2e8b0
bennlich <bennlich@gmail.com>
2015-09-23 16:01:59 -0700

    Add a non-grayscale function to load_image.py for the sake of resize_image.py

--- lib/load_image.py
+++ lib/load_image.py
@@ -6,3 +6,7 @@ def get_image_as_float(img):
 def get_grayscale_image(filename):
   grayscale_image = io.imread(filename, as_grey=True)
   return grayscale_image
+
+def get_image(filename):
+  image = io.imread(filename)
+  return image
