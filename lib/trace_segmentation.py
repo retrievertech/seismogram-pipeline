3a2d2726436843a106600453a3dfb8baa71ba42f
bennlich <bennlich@gmail.com>
2015-09-25 18:24:23 -0700

    Comments and renaming

--- lib/trace_segmentation.py
+++ lib/trace_segmentation.py
@@ -186,74 +186,126 @@ def segments_to_geojson(segments):
   return FeatureCollection(geojson_line_segments)
 
 def get_ridge_line(ridges_h, ridges_v, region):
-  ridge_h_coords = get_ridges_in_region(ridges_h, region)
-  ridge_v_coords = get_ridges_in_region(ridges_v, region)
+  '''
+  Parameters
+  ------------
+  ridges_h : 2-D boolean array
+    Image containing the horizontal ridge lines.
+  ridges_v : 2-D boolean array
+    Image containing the vertical ridge lines.
+  region : 2-D numpy array
+    A list of coordinates defining the region for which you want to
+    generate a center line.
+
+  Returns
+  ---------
+  ridge_line : 2-D numpy array
+    List of coordinates defining the center line of the region.
+
+  '''
+  ridge_h_coords = get_ridge_coords_in_region(ridges_h, region)
+  ridge_v_coords = get_ridge_coords_in_region(ridges_v, region)
   ridge_line = ridges_to_centerline(ridge_h_coords, ridge_v_coords)
   return ridge_line
 
 def get_image_values(img_gray, coords):
   return map(lambda pt : int(255*img_gray[tuple(pt)]), coords)
 
-def get_ridges_in_region(img_ridges, coords):
+def get_ridge_coords_in_region(ridges, coord_list):
   '''
+  Filter through a list of coordinates, returning only those
+  that correspond to a true value in **ridges**.
+
   Parameters
   ------------
-  img_ridges : 2-D boolean array
+  ridges : 2-D boolean array
     The image containing the ridge lines.
-  coords : 2-D numpy array
-    The coordinates that you want to check for ridges.
+  coord_list : 2-D numpy array
+    The coordinates that you want to filter through.
 
   Returns
   ---------
   ridge_coords : numpy array
-    The subset of coords at which img_ridges is true.
+    The subset of coordinates in **coord_list** that correspond
+    to ridges.
+  
   '''
-  coords_mask = img_ridges[coords[:,0], coords[:,1]]
-  ridge_coords = coords[coords_mask]
+  coord_is_a_ridge = ridges[coord_list[:,0], coord_list[:,1]]
+  ridge_coords = coord_list[coord_is_a_ridge]
   return ridge_coords
 
 def ridges_to_centerline(ridge_h_coords, ridge_v_coords):
+  '''
+  Still figuring out the purpose of this function. My current
+  hypothesis is that it limits the use of vertical ridges when
+  calculating the center line coords. I think the goal is to
+  include vertical ridges in centerline estimation *only* when
+  there are horizontal ridges at the same x-coordinate.
+  
+  '''
   if (ridge_h_coords.size == 0) and (ridge_v_coords.size == 0):
     return np.array([[-1, -1]])
 
+  ridge_v_cols = ridge_v_coords[:,1]
+  ridge_h_cols = ridge_h_coords[:,1]
+
   if ridge_v_coords.size > 0:
-    domain_v = np.array([ np.amin(ridge_v_coords[:,1]),
-                          np.amax(ridge_v_coords[:,1]) ], dtype=int)
+    domain_v = np.array([ np.amin(ridge_v_cols), np.amax(ridge_v_cols) ], dtype=int)
     truncate = True
   else:
     truncate = False
 
   if ridge_h_coords.size > 0:
-    domain_h = np.array([np.amin(ridge_h_coords[:,1]),
-               np.amax(ridge_h_coords[:,1])], dtype=int)
+    domain_h = np.array([ np.amin(ridge_h_cols), np.amax(ridge_h_cols) ], dtype=int)
   else:
     domain_h = domain_v[::-1]
 
+  # print ""
+  # print "ridge_v_coords %s" % ridge_v_coords
+  # print "ridge_v_cols %s" % ridge_v_cols
+  # print "ridge_h_cols %s" % ridge_h_cols
+  
   # If ridges vertical at ends, truncate
   if truncate:
+    # print "TRUNCATING RIDGE!"
+    # print "domain_h %s" % domain_h
+    # print "domain_v %s" % domain_v
     if domain_v[0] <= domain_h[0]:
       ridge_v_coords = \
         ridge_v_coords[(ridge_v_coords[:,1] != domain_v[0]),:]
     if domain_v[1] >= domain_h[1]:
       ridge_v_coords = \
         ridge_v_coords[(ridge_v_coords[:,1] != domain_v[1]),:]
-  
+    # print "trunacted ridge_v_coords %s" % ridge_v_coords
+
   ridge = np.vstack((ridge_h_coords, ridge_v_coords))
   return ridge_line_to_series(ridge)
 
 def ridge_line_to_series(coords):
   '''
+  Parameters
+  ------------
+  coords : 2-D int array
+    An array of all pixel coordinates that define the ridge. Coordinates
+    get averaged in the y direction to estimate the center line coords.
+
+  Returns
+  ---------
+  series : 2-D int array
+    An array of all pixel coordinates that define the ridge center line.
 
   '''
   if coords.size == 0:
     return np.array([[0,0]])
-    
-  domain = (min(coords[:,1]), max(coords[:,1]))
+  
+  x_coords = coords[:,1]
+  domain = (min(x_coords), max(x_coords))
   series = []
   for x in range(domain[0], domain[1] + 1):
-    y = coords[coords[:,1] == x][:,0]
-    if y.size > 0:
-      y = np.mean(y)
-      series.append(np.array([y, x]))
+    coords_with_this_x_value = coords[coords[:,1] == x]
+    y_vals = coords_with_this_x_value[:,0]
+    if y_vals.size > 0:
+      avg_y = np.mean(y_vals)
+      series.append(np.array([avg_y, x]))
   series = np.asarray(series)
   return series
