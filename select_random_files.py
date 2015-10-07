bb2b7a1a950cffb0ed81043e6f936752ecbc4781
bennlich <bennlich@gmail.com>
2015-10-07 13:18:53 -0700

    Use test dir

--- select_random_files.py
+++ select_random_files.py
@@ -1,13 +1,14 @@
 import random
 
 num_to_process = 5
+test_dir = "tests/"
 
-with open('filtered_files.txt', 'r') as f:
+with open(test_dir+'filtered_files.txt', 'r') as f:
   lines = f.readlines()
 
 num_files = len(lines)
 random_idxs = random.sample(range(num_files), num_to_process)
 selected_files = [lines[idx] for idx in random_idxs]
 
-with open('selected_files.txt', 'w') as f:
+with open(test_dir+'selected_files.txt', 'w') as f:
   f.writelines(selected_files)
