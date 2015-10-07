bc8293634465a806b87d6c542ca7bee6e1f9a101
bennlich <m@mariusnita.com>
2015-10-07 21:17:28 +0000

    fix and uncomment

--- run_tests.py
+++ run_tests.py
@@ -8,13 +8,14 @@ with open(test_dir+'selected_files.txt', 'r') as f:
 print filenames
 num_to_process = len(filenames)
 
-#for (i, filename.rtsrip()) in enumerate(filenames):
-#  print "\nimage %s of %s" % (i, num_to_process)
-#  local_path = test_dir+filename
-#  call(["aws s3 cp \"s3://WWSSN_Scans/"+filename+"\" ./"+local_path+" --region us-east-1"], shell=True)
-#
-#  print "running test_meanlines_and_roi.sh"
-#  call(["bash test_meanlines_and_roi_s3.sh "+filename+" "+local_path], shell=True)
-#
-#  print "deleting %s" % filename
-#  call(["rm "+local_path], shell=True)
+for (i, filename) in enumerate(filenames):
+  filename = filename.rstrip()
+  print "\nimage %s of %s" % (i, num_to_process)
+  local_path = test_dir+filename
+  call(["aws s3 cp \"s3://WWSSN_Scans/"+filename+"\" ./"+local_path+" --region us-east-1"], shell=True)
+
+  print "running test_meanlines_and_roi.sh"
+  call(["bash test_meanlines_and_roi_s3.sh "+filename+" "+local_path], shell=True)
+
+  print "deleting %s" % filename
+  call(["rm "+local_path], shell=True)
