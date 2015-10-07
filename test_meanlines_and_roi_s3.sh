421d3c5c7a9bd8e5997f38ab38ced77e59c8797e
Marius Nita <m@mariusnita.com>
2015-10-07 20:11:23 +0000

    fix server url and auth

--- test_meanlines_and_roi_s3.sh
+++ test_meanlines_and_roi_s3.sh
@@ -8,7 +8,7 @@
 # otherwise it's copied to the s3 metadata bucket.
 
 # node server
-server=http://seismo.redfish.com
+server=http://seismo.redfish.com:3000
 # "xxxxxx_xxxx_xxxx_xx.png"
 image_name=$1
 # full path to the image
@@ -16,10 +16,13 @@ image_path=$2
 # "dev" is development, everything else is production
 type=$3
 
+user=seismo
+pass=Redfish32
+
 dir=`mktemp -d /tmp/seismo.XXXXX` && \
 echo "writing to $dir" && \
-wget $server/processing/setstatus/$image_name/1 -O /dev/null && \
-python test_meanlines_and_roi_s3.py --image "$image_path" --output "$dir" && \
+wget --user $user --password $pass $server/processing/setstatus/$image_name/1 -O /dev/null && \
+python test_meanlines_and_roi.py --image "$image_path" --output "$dir" --stats stats.json && \
 sh copy_to_s3.sh $image_name $dir normal $type && \
-wget $server/processing/setstatus/$image_name/3 -O /dev/null && \
+wget --user $user --password $pass $server/processing/setstatus/$image_name/3 -O /dev/null && \
 rm -rf $dir
