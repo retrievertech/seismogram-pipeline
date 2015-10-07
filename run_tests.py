f200777d7be9e86d9de1a49ab0ac3c7930da0eff
bennlich <bennlich@gmail.com>
2015-10-07 13:07:57 -0700

    Split run_tests into two files; save list of randomly selected filenames

--- run_tests.py
+++ run_tests.py
@@ -1,20 +1,16 @@
-import random
 from subprocess import call
 
-f = open('filtered_files.txt', 'r')
-lines = f.readlines()
-num_files = len(lines)
+with open('selected_files.txt', 'r') as f:
+  filenames = f.readlines()
 
-num_to_process = 5
-random_idxs = random.sample(range(num_files), num_to_process)
+print filenames
 
-for idx in random_idxs:
-  filename = lines[idx].rstrip()
-  print "copying %s from s3" % filename
-  call(["aws s3 cp \"s3://WWSSN_Scans/"+filename+"\" ./"+filename+" --region us-east-1"], shell=True)
+# for filename.rstrip() in filenames:
+#   print "copying %s from s3" % filename
+#   call(["aws s3 cp \"s3://WWSSN_Scans/"+filename+"\" ./"+filename+" --region us-east-1"], shell=True)
 
-  # print "running test_meanlines_and_roi.sh"
-  # call(["bash test_meanlines_and_roi_s3.sh", filename, filename], shell=True)
+#   # print "running test_meanlines_and_roi.sh"
+#   # call(["bash test_meanlines_and_roi_s3.sh", filename, filename], shell=True)
 
-  print "deleting %s" % filename
-  call(["rm "+filename], shell=True)
+#   print "deleting %s" % filename
+#   call(["rm "+filename], shell=True)
