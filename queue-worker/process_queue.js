65acd55fa00aeb2f1d03c30b3716d28e969ea5a1
bennlich <bennlich@gmail.com>
2015-10-23 12:38:45 -0700

    More work on process_queue

--- queue-worker/process_queue.js
+++ queue-worker/process_queue.js
@@ -1,84 +1,93 @@
 var Queue = require('firebase-queue'),
     Firebase = require("firebase"),
     exec = require("child_process").exec,
-    rootRef = require("./rootRef");
+    fs = require("fs");
 
 // post machine presence to firebase
 require("./presence");
+
+var rootRef = require("./rootRef");
+var logPath = __dirname + "/logs";
 
 var queueRef = rootRef.child("queue");
 var queue = new Queue(queueRef, function(data, progress, resolve, reject) {
   // Read and process task data 
   console.log(data);
  
+  var filename = data.filename;
+
+  processSeismo(filename, cb);
+
   // Do some work 
-  progress(50);
+  // progress(50);
  
   // Finish the task asynchronously 
-  setTimeout(function() {
-    if (Math.random() < 0.5) {
-      resolve();
-    }
-    else {
-      reject();
-    }
-  }, 5000);
+  // setTimeout(function() {
+  //   if (Math.random() < 0.5) {
+  //     resolve();
+  //   }
+  //   else {
+  //     reject();
+  //   }
+  // }, 5000);
 });
 
-// exec(command, {maxBuffer: 1024 * 1000}, function(err, stdout, stderr) {
-//   if (err) {
-//     console.log(stdout);
-//     console.log(stderr);
-//     res.status(503).send({error: err.message});
-//   } else {
-//     console.timeEnd("running assignment...");
-//     var assign = fs.readFileSync(path + "/assignments.json");
-//     res.send(assign);
-//   }
-// });
+var processSeismo = function(filename, callback) {  
+  var command = "sh process_task.sh " + filename;
 
-// var writeLog = function(filename, logContents) {
-//   if (!fs.existsSync(logPath)) {
-//     fs.mkdirSync(logPath);
-//   }
+  // if (process.env.NODE_ENV !== "production") {
+  //   command += " dev";
+  // }
 
-//   var path = logPath + "/" + filename + ".txt";
+  exec(command, function(err, stdout, stderr) {
+    var log = "== stdout ==\n";
+    log += stdout;
+    log += "\n== stderr ==\n";
+    log += stderr;
 
-//   fs.writeFile(path, logContents, function(err) {
-//     if (err) {
-//       console.log("error writing to log", filename, err);
-//     }
-//   });
-// };
+    if (err) {
+      setStatus(filename, status.failed);
+    }
 
-// router.get("/start/:filename", function(req, res) {
-//   var filename = req.params.filename;
-//   var path = diskCache.localPath(filename);
+    console.log(log);
+    writeLog(filename, log);
+  });
+}
 
-//   // return immediately
-//   res.send({ ok: 1 });
+var writeLog = function(filename, logContents) {
+  if (!fs.existsSync(logPath)) {
+    fs.mkdirSync(logPath);
+  }
 
-//   diskCache.ensureFileIsLocal(filename, function() {
-//     process.chdir(pipelinePath);
+  var path = logPath + "/" + filename + ".txt";
 
-//     var command = "sh get_all_metadata_s3.sh " + filename + " " + escape(path);
+  fs.writeFile(path, logContents, function(err) {
+    if (err) {
+      console.log("error writing to log", filename, err);
+      return;
+    }
+    uploadLog(filename, path);
+  });
+};
 
-//     if (process.env.NODE_ENV !== "production") {
-//       command += " dev";
-//     }
+var uploadLog = function(filename, path) {
+  var command = "sh copy_to_s3.sh " + filename + " " + path + " logs";
+  console.log(command);
+  exec(command, function(err) {
+    if (err) {
+      console.log("error copying log to s3", err);
+      return;
+    }
+  });
+}
 
-//     exec(command, function(err, stdout, stderr) {
-//       var log = "== stdout ==\n";
-//       log += stdout;
-//       log += "\n== stderr ==\n";
-//       log += stderr;
-
-//       if (err) {
-//         setStatus(filename, status.failed);
-//       }
-
-//       console.log(log);
-//       writeLog(filename, log);
-//     });
-//   });
-// });
+var setStatus = function(filename, status) {
+  var command = "sh set_seismo_status.sh " + filename + " " + status;
+  console.log(command);
+  exec(command, function(err) {
+    if (err) {
+      console.log("error setting seismo status", err);
+      return;
+    }
+  });
+}
