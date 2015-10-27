8225360587e5d9421f96de9538ed4bf9ca41779c
bennlich <bennlich@gmail.com>
2015-10-27 14:18:04 -0700

    Refactor launch_instances; accept num_instances as a command line argument

--- queue-worker/launch_instances.py
+++ queue-worker/launch_instances.py
@@ -1,25 +1,52 @@
+"""
+Description:
+  Launch EC2 r3.xlarge (30.5GB RAM) machines that will immediately begin
+  processing the seismogram queue.
+
+Usage:
+  launch_instances.py --num <num_instances>
+  launch_instances.py -h | --help
+
+Options:
+  -h --help              Show this screen.
+  --num <num_instances>  Number of instances to launch.
+
+"""
+
+from docopt import docopt
 import boto3
-ec2 = boto3.resource('ec2')
 
-# id for the machine image that has all of
-# our dependencies installed
-ami_image_id = 'ami-dfa1eeba'
+def launch_instances(num_instances):
+  ec2 = boto3.resource('ec2')
 
-# id for the security group that permits ssh access
-# from all IP addresses
-security_group_ids = ['sg-1b34cb7d']
+  # id for the machine image that has all of
+  # our dependencies installed
+  ami_image_id = 'ami-0027566a'
 
-instance_type = 'r3.large'
+  # id for the security group that permits ssh access
+  # from all IP addresses
+  security_group_ids = ['sg-1b34cb7d']
 
-# the user_data script is run once when a new machine boots
-with open("user_data.sh", "r") as myfile:
-  user_data = myfile.read()
+  instance_type = 'r3.xlarge'
 
-print "Creating instances..."
-new_instance_ids = ec2.create_instances(ImageId=ami_image_id,
-                     MinCount=1, MaxCount=1,
-                     SecurityGroupIds=security_group_ids,
-                     InstanceType=instance_type,
-                     UserData=user_data)
+  # the user_data script is run once when a new machine boots
+  with open("user_data.sh", "r") as myfile:
+    user_data = myfile.read()
+    
+  print "Creating instances..."
+  new_instance_ids = ec2.create_instances(ImageId=ami_image_id,
+                       MinCount=num_instances, MaxCount=num_instances,
+                       SecurityGroupIds=security_group_ids,
+                       InstanceType=instance_type,
+                       UserData=user_data)
 
-print "created instances: %s" % new_instance_ids
+  print "created instances: %s" % new_instance_ids
+
+if __name__ == '__main__':
+  arguments = docopt(__doc__)
+  num_instances = int(arguments["--num"])
+  
+  if (num_instances >= 1):
+    launch_instances(num_instances)
+  else:
+    print(arguments)
