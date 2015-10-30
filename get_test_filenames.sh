51c23fc10009d889f0affa4358b93ef600894e03
bennlich <bennlich@gmail.com>
2015-10-30 15:15:07 -0700

    More filtering; comments

--- get_test_filenames.sh
+++ get_test_filenames.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
+# These regexs don't work on my local OSX machine,
+# but they work on the ec2 ubuntu machines.
+
+# I haven't tested the latest version of this script,
+# but I have tested the regexs.
+
 dir=tests
 
 mkdir -p $dir 
 
 aws s3 ls "s3://WWSSN_Scans" --region us-east-1 > $dir/ls_WWSSN_Scans.txt
 
-regex='s/.*\s.*\s\([0-9]\{4\}[7-9][0-9]_[0-9]\{4\}_[0-9]\{4\}_0[456]\.png\).*/\1/p'
+regex1='s/.*\s.*\s\([0-9]\{4\}[7-9][0-9]_[0-9]\{4\}_[0-9]\{4\}_0[456]\.png\).*/\1/p'
 # get all filenames of the form xxxx[# >= 70]_xxxx_xxxx_[04, 05, or 06].png
 # i.e. only long-period seismos after 1970
 
-sed -n $regex <$dir/ls_WWSSN_Scans.txt  >$dir/filtered_files.txt
+regex2='/[0-9]\{6\}_[0-9]\{4\}_0009_0[456]\.png/d'
+# delete all lines with station id 0009
+
+sed -n $regex1 <$dir/ls_WWSSN_Scans.txt  >$dir/temp.txt
+sed $regex2 <$dir/temp.txt  >$dir/filtered_files.txt
