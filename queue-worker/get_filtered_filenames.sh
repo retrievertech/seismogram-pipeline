845b7bfd67635620e4aa8350ae698354682e5d78
bennlich <bennlich@gmail.com>
2015-10-30 15:16:44 -0700

    Remove directory

--- queue-worker/get_filtered_filenames.sh
+++ queue-worker/get_filtered_filenames.sh
@@ -6,11 +6,7 @@
 # I haven't tested the latest version of this script,
 # but I have tested the regexs.
 
-dir=tests
-
-mkdir -p $dir 
-
-aws s3 ls "s3://WWSSN_Scans" --region us-east-1 > $dir/ls_WWSSN_Scans.txt
+aws s3 ls "s3://WWSSN_Scans" --region us-east-1 > ls_WWSSN_Scans.txt
 
 regex1='s/.*\s.*\s\([0-9]\{4\}[7-9][0-9]_[0-9]\{4\}_[0-9]\{4\}_0[456]\.png\).*/\1/p'
 # get all filenames of the form xxxx[# >= 70]_xxxx_xxxx_[04, 05, or 06].png
@@ -19,5 +15,5 @@ regex1='s/.*\s.*\s\([0-9]\{4\}[7-9][0-9]_[0-9]\{4\}_[0-9]\{4\}_0[456]\.png\).*/\
 regex2='/[0-9]\{6\}_[0-9]\{4\}_0009_0[456]\.png/d'
 # delete all lines with station id 0009
 
-sed -n $regex1 <$dir/ls_WWSSN_Scans.txt  >$dir/temp.txt
-sed $regex2 <$dir/temp.txt  >$dir/filtered_files.txt
+sed -n $regex1 <ls_WWSSN_Scans.txt  >temp.txt
+sed $regex2 <temp.txt  >filtered_files.txt
