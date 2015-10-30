90832651c0eeadf55a14328acc02d65ca44836c3
bennlich <bennlich@gmail.com>
2015-10-30 15:17:47 -0700

    Remove directory args again

--- select_random_files.py
+++ select_random_files.py
@@ -1,14 +1,13 @@
 import random
 
 num_to_process = 100
-test_dir = "tests/"
 
-with open(test_dir+'filtered_files.txt', 'r') as f:
+with open('filtered_files.txt', 'r') as f:
   lines = f.readlines()
 
 num_files = len(lines)
 random_idxs = random.sample(range(num_files), num_to_process)
 selected_files = [lines[idx] for idx in random_idxs]
 
-with open(test_dir+'selected_files.txt', 'w') as f:
+with open('selected_files.txt', 'w') as f:
   f.writelines(selected_files)
