1ab1bd0d892c7b85a005126ee12e36e44f7db4ad
bennlich <bennlich@gmail.com>
2015-10-30 15:21:31 -0700

    Print to stdout instead of writing to file

--- queue-worker/select_random_files.py
+++ queue-worker/select_random_files.py
@@ -9,5 +9,4 @@ num_files = len(lines)
 random_idxs = random.sample(range(num_files), num_to_process)
 selected_files = [lines[idx] for idx in random_idxs]
 
-with open('selected_files.txt', 'w') as f:
-  f.writelines(selected_files)
+print selected_files
