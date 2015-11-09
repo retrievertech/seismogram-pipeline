8d59bce7a9a2085605ce7874e694fc81bfc3cc57
bennlich <bennlich@gmail.com>
2015-11-08 17:39:42 -0800

    comment

--- queue-tools/user_data.sh
+++ queue-tools/user_data.sh
@@ -4,4 +4,4 @@ sudo -H -u ubuntu bash -c 'git pull' && \
 sudo -H -u ubuntu bash -c 'node ./queue-worker/process_queue.js'
 
 # output of this script is written to:
-# cat /var/log/cloud-init-output.log
+# /var/log/cloud-init-output.log
